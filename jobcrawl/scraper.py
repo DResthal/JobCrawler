4cd944468b82cf7fc7473f014617463fadba74a1
NeilClack <c.neilclack@gmail.com>
2022-11-08 07:24:40 -0600

    cleanup

--- jobcrawl/scraper.py
+++ jobcrawl/scraper.py
@@ -1,17 +1,30 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-firefox_Options = webdriver.FirefoxOptions()
-firefox_Options.headless = True
-browser = webdriver.Firefox(options=firefox_Options)
-browser.get("https://www.indeed.com/jobs?q=Data+Analyst&l=Remote")
 
-source = browser.page_source
 
-browser.close()
+def make_request():
+    firefox_Options = webdriver.FirefoxOptions()
+    firefox_Options.headless = True
+    browser = webdriver.Firefox(options=firefox_Options)
+    browser.get("https://www.indeed.com/jobs?q=Data+Analyst&l=Remote")
 
-soup = BeautifulSoup(source, 'lxml')
+    source = browser.page_source
+    browser.close()
+    soup = BeautifulSoup(source, 'lxml')
+    get_item_list(soup)
 
-ls = soup.find_all('ul', attrs='jobsearch-ResultsList')
 
-print(ls)
+def get_item_list(soup):
+    job_list = soup.find('ul', attrs='jobsearch-ResultsList')
+    list_items = job_list.find_all('li')
+
+    parse_jobs(list_items)
+
+def parse_jobs(items):
+    for i in items:
+        print(i.find('div', attrs='cardOutline'))
+
+
+if __name__ == "__main__":
+    make_request()
