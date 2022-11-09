8391e4ecf3104fcc687bc5c67f778a8d959f1ec9
NeilClack <c.neilclack@gmail.com>
2022-11-09 10:10:57 -0600

    Rebuild data extraction and complete date cleaning

--- jobcrawl/scraper.py
+++ jobcrawl/scraper.py
@@ -1,97 +1,151 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 import json
+import re
 
 
 def make_request(page):
     firefox_Options = webdriver.FirefoxOptions()
     firefox_Options.headless = True
     browser = webdriver.Firefox(options=firefox_Options)
-    browser.get("https://www.indeed.com/jobs?q=Data+Analyst&l=Remote&sort=date&fromage=3" + '&start' + str(page * 10))
+
+    if page >= 1:
+        url = (
+            "https://www.indeed.com/jobs?q=Data+Analyst&l=Remote&fromage=7"
+            + "&start"
+            + str(page * 10)
+        )
+    else:
+        url = "https://www.indeed.com/jobs?q=Data+Analyst&l=Remote&fromage=7"
+
+    browser.get(url)
 
     source = browser.page_source
     browser.close()
-    soup = BeautifulSoup(source, 'lxml')
-    get_item_list(soup)
+    soup = BeautifulSoup(source, "lxml")
+
+    get_jobs(soup.find("div", attrs={"class": "mosaic-provider-jobcards"}))
 
 
-def get_item_list(soup):
-    job_list = soup.find('ul', attrs='jobsearch-ResultsList')
-    with open('dump.html', 'w') as f:
-        f.write(str(job_list.prettify))
-    extract_info(job_list)
+def get_jobs(posts):
+    job_list = []
+    for i in posts.find("ul"):
 
-def extract_info(items):
-    # Iterate over all <li> elements in the list
-    for i in items:
+        # Job Title
+        # All fields are nested within this "if" to avoid pulling every 5th entry (None) as a duplicate
+        if i.find("h2", attrs={"class": "jobTitle"}) != None:
+            title = i.find("h2", attrs={"class": "jobTitle"}).text
 
-        # Main card content
-        table = i.find('table', attrs='jobCard_mainContent')
+            # Company
 
-        if table != None:
-
-            # Job Title
-            title = table.find('h2', attrs='jobTitle').span.contents[0]
-
-            # Company Name
-            company_name = table.find('span', attrs='companyName').string
+            if i.find("span", attrs="companyName") != None:
+                company = i.find("span", attrs="companyName").text
 
             # Indeed URL
-            url = 'www.indeed.com' + table.find('h2', attrs={'class':'jobTitle'}).a['href']
+
+            if i.find("h2", attrs={"class": "jobTitle"}).a != None:
+                url = i.find("h2", attrs={"class": "jobTitle"}).a["href"]
 
             # Location
-            location = table.find('div', attrs='companyLocation').string
+            if i.find("div", attrs={"class": "companyLocation"}) != None:
+                location = i.find("div", attrs={"class": "companyLocation"}).string
 
             # Salary
-            if table.find('div', {'class':'salaryOnly'}) != None:
-
-                salary_only = table.find('div', attrs='salaryOnly')
-
-                try:
-                    salary_only.svg.extract()
-                except:
-                    pass
-                
-                if salary_only.contents[0].text != None:
-                    salary = salary_only.contents[0].text
-                else:
-                    salary = ''
+            if i.find("div", attrs={"class": "salaryOnly"}) != None:
+                salary = i.find("div", attrs={"class": "salaryOnly"}).text
             else:
-                salary = ''
+                salary = None
 
-            # Card Footer
-            footer = i.find('div', attrs={'class':'result-footer'})
-
-            if footer != None:
+            # Footer
+            if i.find("tr", attrs={"class": "underShelfFooter"}) != None:
+                footer = i.find("tr", attrs={"class": "underShelfFooter"})
 
                 # Short Description
-                short_desc = footer.find('div', {'class': 'job-snippet'}).text
+                if footer.find("div", attrs={"class": "job-snippet"}) != None:
+                    short_description = footer.find(
+                        "div", attrs={"class": "job-snippet"}
+                    ).text
 
-                # Date posted
-                if footer.find('span', {'class':'date'}) != None:
-                    date = footer.find('span', {'class' : 'date'}).text.replace('Posted', '')
+                # Posted Date
+                if footer.find("span", attrs={"class": "date"}) != None:
+                    date = str(
+                        footer.find("span", attrs={"class": "date"}).text
+                    ).replace("Posted", "")
 
-        job_dict_raw = {
-            'title' : title,
-            'company' : company_name,
-            'url' : url,
-            'location' : location,
-            'salary' : salary,
-            'summary' : short_desc,
-            'date' : date
-        }
+            job_listing = {
+                "title": title,
+                "company": company,
+                "url": url,
+                "location": location,
+                "salary": salary,
+                "short_description": short_description,
+                "date_posted": date,
+            }
 
-        print(json.dumps(job_dict_raw, indent=2, sort_keys=False))
+            job_listing = clean_job(job_listing)
 
-def clean_job(job):
-    for k, v in job:
-        print(k + " : " + v)
+            job_list.append(job_listing)
 
-    print(job)
+    # print(json.dumps(job_list, indent=2, sort_keys=False))
 
+
+def clean_job(job: dict = None) -> dict:
+    """Input = Job dict
+
+    Convert dates to datetime objects.\n
+    Convert salary to integers.\n
+    Remove leading and trailing whitespace from strings.\n
+    escape all strings.\n
+    Remove line-breaks from strings.\n
+    """
+
+    job["date_posted"] = convert_date(job["date_posted"])
+    job["salary"] = extract_salary(job["salary"])
+
+    for k, v in job.items():
+        if k == "salary":
+            continue
+
+        if k == "date_posted":
+            continue
+
+        if v == None:
+            continue
+        else:
+            v = job[k].strip().replace("\n", "")
+
+        job[k] = v
+
+
+def convert_date(d):
+
+    # Convert "just posted" and "today"
+    if d.lower() == "just posted":
+        d = datetime.today()
+        return d
+
+    if d.lower() == "today":
+        d = datetime.today()
+        return d
+
+    if d.lower() == "hiring ongoing":
+        d = datetime.today()
+        return d
+
+    d = re.findall("\d+", d)
+    d = datetime.today() - timedelta(days=int(str("".join(d))))
+    d = d.strftime("%m-%d-%Y")
+
+    return d
+
+
+def extract_salary(s):
+    return s
 
 
 if __name__ == "__main__":
-    pages = 5
+    pages = 1
     for page in range(pages):
+        print(f"Working on page: {page + 1}")
         make_request(page)
