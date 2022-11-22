e8b67c4a51400fb55e64b7ac6484d698da131612
NeilClack <c.neilclack@gmail.com>
2022-11-22 11:34:57 -0600

    Create jobcard item

--- JobCrawler/JobCrawler/items.py
+++ JobCrawler/JobCrawler/items.py
@@ -6,7 +6,13 @@
 import scrapy
 
 
-class JobcrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class JobCardItem(scrapy.Item):
+    title = scrapy.Field()
+    company = scrapy.Field()
+    url = scrapy.Field()
+    location = scrapy.Field()
+    salary = scrapy.Field()
+    summary = scrapy.Field()
+    posted = scrapy.Field()
+    scraped = scrapy.Field()
     pass
